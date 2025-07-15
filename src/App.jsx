d0cf7a035ca1f55e148e855ea202708f56d4f7b4
Figmauchun <figmauchun123@gmail.com>
2025-07-15 14:08:10 +0500

    sayt yuklanganda ochish animatsiyasi

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./pages/Home";
@@ -6,8 +6,31 @@ import AddBlog from "./pages/AddBlog";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import BlogDetails from "./pages/BlogDetails";
 import EditBlog from "./pages/EditBlog";
+import ClipLoader from "react-spinners/ClipLoader"; // 🔄 Spinner
+import { BounceLoader } from "react-spinners";
 
 function App() {
+  const [loading, setLoading] = useState(true); // Spinner holati
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 3000); // ⏱ 3 soniya
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-screen flex-col gap-4 bg-white">
+        <BounceLoader color="#2563eb" size={60} />
+        <h2 className="text-2xl font-semibold text-blue-700 animate-pulse">
+          Xush kelibsiz!
+        </h2>
+      </div>
+    );
+  }
+
   return (
     <>
       <BrowserRouter>
